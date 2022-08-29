afbb33e043dc0168e68e005197aded95654fe6a5
Eronwuka <105295233+Eronwuka@users.noreply.github.com>
2022-08-29 21:52:11 +0100

    second commit

---
+++ script.js
@@ -0,0 +1,27 @@
+
+let saveEl = document.getElementById("save-el")
+console.log(saveEl)
+
+let countEl = document.getElementById("count-el")
+
+console.log(countEl)
+
+let count = 0
+
+function increment() {
+    count += 1
+    countEl.textContent = count
+}
+
+function save() {
+    let countstr = count + " - "
+    saveEl.textContent += countstr
+    countEl.textContent = 0 
+    count = 0   
+}
+
+
+
+
+
+
